18ea3360482e2e154f2c2843b4bf9641d5aef87a
David Hollis <dhollis@davehollis.com>
2020-04-14 09:48:16 -0400

    Add PHPUnit functional tests

---
+++ tests/DashboardApiTest.php
@@ -0,0 +1,95 @@
+<?php
+namespace Meraki\Api\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Meraki\Api\Client;
+use Meraki\Api\Authentication\CiscoMerakiAPIKeyAuthentication;
+use Meraki\Api\Model\Organization;
+use Meraki\Api\Model\Network;
+use Meraki\Api\Model\SmUser;
+use Meraki\Api\Model\SmProfile;
+use Meraki\Api\Model\SmDevice;
+use Meraki\Api\Model\SmConnectivity;
+use Meraki\Api\Model\SmSecurityCenter;
+use Meraki\Api\Model\NetworksNetworkIdSmDevicesGetResponse200;
+use Meraki\Api\Model\NetworksNetworkIdSmProfilesGetResponse200;
+
+class DashboardApiTest extends TestCase
+{
+    private function getClient(): Client
+    {
+        $auth = new CiscoMerakiAPIKeyAuthentication($_SERVER['API_TOKEN']);
+        
+        return Client::create(null, $auth);
+    }
+    
+    public function testListOrgSuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getOrganizations();
+        
+        self::assertNotEmpty($response);
+        self::assertInstanceOf(Organization::class, $response[0]);
+    }
+    
+    public function testListNetworkSuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getOrganizationNetworks($_SERVER['ORG_ID']);
+        
+        self::assertNotEmpty($response);
+        self::assertInstanceOf(Network::class, $response[0]);
+    }
+    
+    public function testListSmUserSuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getNetworkSmUsers($_SERVER['NETWORK_ID']);
+        
+        self::assertNotEmpty($response);
+        self::assertInstanceOf(SmUser::class, $response[0]);
+    }
+    
+    public function testListSmProfileSuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getNetworkSmProfiles($_SERVER['NETWORK_ID']);
+        
+        self::assertInstanceOf(NetworksNetworkIdSmProfilesGetResponse200::class, $response);
+        self::assertNotEmpty($response->getProfiles());
+    }
+    
+    public function testListSmDeviceSuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getNetworkSmDevices($_SERVER['NETWORK_ID']);
+        
+        self::assertInstanceOf(NetworksNetworkIdSmDevicesGetResponse200::class, $response);
+        self::assertNotEmpty($response->getDevices());
+    }
+    
+    public function testListSmDeviceConnectivitySuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getNetworkSmConnectivity($_SERVER['NETWORK_ID'], $_SERVER['SM_DEVICE_ID']);
+        
+        self::assertNotEmpty($response);
+        self::assertInstanceOf(SmConnectivity::class, $response[0]);
+    }
+    
+    public function testListSmDeviceSecuritySuccess()
+    {
+        $client = $this->getClient();
+        
+        $response = $client->getNetworkSmSecurityCenters($_SERVER['NETWORK_ID'], $_SERVER['SM_DEVICE_ID']);
+        
+        self::assertNotNull($response);
+        self::assertInstanceOf(SmSecurityCenter::class, $response[0]);
+    }
+}
